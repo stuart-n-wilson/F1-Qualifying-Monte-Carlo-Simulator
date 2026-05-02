8bb124051d702239c97168eb33a0f1e60f1ab9db
Stuart Wilson <stuwilson98@gmail.com>
2026-05-02 19:59:22 +0100

    Add files via upload

--- pages/Qualifying_simulator.py
+++ pages/Qualifying_simulator.py
@@ -3,8 +3,9 @@ import streamlit as st
 import fastf1 as f1
 import pandas as pd
 from datetime import datetime as dt
-from simulator import monte_carlo_qualifying
+from simulator import monte_carlo_qualifying, simulate_grid
 from plotting import position_probability_plot, expected_position, create_heatmap
+from analysis import compare_grid
 
 f1.set_log_level('ERROR')
 
@@ -23,7 +24,7 @@ gp = st.selectbox("Grand Prix", f1.get_event_schedule(year, include_testing=Fals
 
 
 # Load session and cache ---
-@st.cache_resource(show_spinner=False)
+@st.cache_resource(show_spinner="Downloading the data...")
 def load_session(year, gp):
     f1.set_log_level('ERROR')
     session = f1.get_session(year, gp, 'Q')
@@ -38,14 +39,14 @@ n = st.number_input("Monte Carlo simulations", min_value=1, max_value=5000, valu
 st.divider()
 
 st.subheader("Run the simulation")
-st.info("It might take a while, the icon in the top right will show that it is running...")
 
 # Run simulation ---
 if st.button("Run"):
-    st.session_state.df = monte_carlo_qualifying(session, n)
-    st.session_state.n = n
-    st.session_state.run_year = year
-    st.session_state.run_gp = gp
+    with st.spinner("Running the simulation..."):
+        st.session_state.df = monte_carlo_qualifying(session, n)
+        st.session_state.n = n
+        st.session_state.run_year = year
+        st.session_state.run_gp = gp
 
 st.divider()
 
@@ -57,31 +58,54 @@ if (
 
     st.subheader("Simulation Results")
 
-    tab1, tab2, tab3, tab4 = st.tabs(["Dataframe", "Heatmap", "Position Analysis","Driver Analysis"])
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Simulated results", "Position Analysis","Driver Analysis", "Probabilities dataframe", "Probabilties heatmap"])
 
-    # Dataframe
+    # Simulated results
     with tab1:
-        st.dataframe(st.session_state.df)
+        st.dataframe(compare_grid(simulate_grid(st.session_state.df), session))
+        st.info("A real position may be None if the driver did not qualify.", icon="ℹ️")
 
-    # Heatmap
+    # Position probability distribution plot
     with tab2:
-        fig = create_heatmap(st.session_state.df, session)
-        st.plotly_chart(fig, use_container_width=True)
-    
-    # Position
-    with tab3:
         pos = st.slider("Qualifying position", min_value=1, max_value=len(session.results), value=1)
+
+        # Protect against missing driver for position.
+        match = session.results.loc[session.results['Position'].astype('Int64') == pos, 'FullName'].values
+        if len(match) == 0:
+            st.markdown(f"Real P{pos} qualifier: No driver qualified in this position")
+        else:
+            st.markdown(f"Real P{pos} qualifier: {match[0]}")
+
         fig = position_probability_plot(st.session_state.df, session, pos, st.session_state.n)
         st.plotly_chart(fig, use_container_width=True)
     
-    # Driver
-    with tab4:
+    # Driver probability distribution plot
+    with tab3:
         driver = st.selectbox("Driver", sorted(session.get_driver(d)['FullName'] for d in session.drivers))
-        abbr = session.results.loc[
-            session.results["FullName"] == driver,
-            "Abbreviation"].values[0]
+        abbr = session.results.loc[session.results["FullName"] == driver, "Abbreviation"].values[0]
+        position = session.results.loc[session.results['FullName'] == driver, 'Position'].values[0]
+
+        if pd.isna(position):
+            st.markdown("Real qualifying position: did not qualify.")
+        else:
+            st.markdown(f"Real qualifying position: P{int(position)}.")
+
         fig = expected_position(st.session_state.df, session, driver, abbr, st.session_state.n)
         st.plotly_chart(fig, use_container_width=True)
 
+    # Dataframe of probabilities
+    with tab4:
+        # Rename column headers to P1, P2 etc.
+        st.dataframe(st.session_state.df.rename(columns=lambda x: f"P{x}"))
+        st.info("This table shows the probability (0 - 1) of a driver qualifying in each position.", icon="ℹ️")
+
+    # Probabilities heatmap
+    with tab5:
+        fig = create_heatmap(st.session_state.df, session)
+        st.plotly_chart(fig, use_container_width=True)
+        st.info("This is the Probabilties dataframe as a heatmap.", icon="ℹ️")
+
+
+# Display message if session changes that simulation must be rerun.
 elif "df" in st.session_state:
-    st.info("Session changed — click Run to generate results for the selected qualifying session.")
+    st.info("Session changed — click Run to generate results for the selected qualifying session.", icon="ℹ️")
